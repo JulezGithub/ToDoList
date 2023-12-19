c5aa1f096614196e6ee6dcaa807adb1e237aed2b
JulezGit <julez.weisser@gmail.com>
2023-12-19 14:44:00 +0100

    first commit

---
+++ ToDoList.py
@@ -0,0 +1,16 @@
+class FileHandler:
+    def __init__(self):
+        pass
+
+
+
+
+class ToDoList:
+    def __init__(self):
+        self.todolist = []
+
+
+
+class Entry:
+    def __init__(self):
+        pass
